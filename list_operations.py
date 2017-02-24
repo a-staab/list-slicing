909724eac1c05d7575152667b928d2cf1df4f9b1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-02-23 17:38:20 -0800

    Tidied up

--- list_operations.py
+++ list_operations.py
@@ -147,7 +147,7 @@ def inner_four_end(input_list):
 
     """
 
-    return input_list[-6:-2:1]
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -179,8 +179,7 @@ def replace_third_and_last(input_list):
     True
 
     """
-    input_list[2] = 37
-    input_list[-1] = 37
+    input_list[2], input_list[-1] = 37, 37
 
 
 def replace_middle(input_list):
